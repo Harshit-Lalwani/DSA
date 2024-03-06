b7655c7d55e7bb7f1e32f4f28954ee2ae5687326
Harshit-Lalwani <143736548+Harshit-Lalwani@users.noreply.github.com>
2024-03-06 17:46:42 +0000

    retry

---
+++ Assignments/A3_retry/q1.c
@@ -0,0 +1,184 @@
+// AIM: for a given array, find the longest subsequence of consecutive increasing numbers
+
+//Constraints: 1<=n<=10^5, 1<=arr[i]<=10^9
+// LOGIC:
+// Implement hashtable to store int size and an array of Sentinel nodes(all initialized to poison values) 
+// Each sentinel node contains int final_value, int lowest_value, count, Node* first
+// first is the pointer to the first node of the linkedlist that stores the subsequence.
+// Each Node caontains int value, int index(index from orgiinal array) Node* next
+// for each element in the array, check if it is the successor of the last element of a subsequence. If yes, then create a new node and add it to the linkedlist of the corresponding sentinel node. Otherwise, if element does not already exist in any of the subsequences, add it to the linkedlist of (size of table)th sentinel node and then incremkent the size of the table.
+// Finally, find the sentinel node with the maximum count and print the linkedlist of indices that sentinel node.
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX 300000
+
+typedef struct Node
+{
+    int value;
+    int index;
+    struct Node* next;
+}Node;
+
+typedef struct Sentinel
+{
+    int final_value;
+    int lowest_value;
+    int count;
+    Node* first;
+}Sentinel;
+
+typedef struct HashTable
+{
+    int size;
+    Sentinel* table[MAX];
+}HashTable;
+
+void init(HashTable* h)
+{
+    h->size = 0;
+    for(int i=0; i<MAX; i++)
+    {
+        h->table[i] = (Sentinel*)malloc(sizeof(Sentinel));
+        if(h->table[i] == NULL)
+        {
+            printf("Memory allocation failed\n");
+            exit(1);
+        }
+        h->table[i]->final_value = -1;
+        h->table[i]->lowest_value = -1;
+        h->table[i]->count = 0;
+        h->table[i]->first = NULL;
+    }
+}
+
+
+// function to insert node at end of linkedlist corresponding to kth sentinel node
+void insert_to_ll(int value, int index, int k, HashTable* h)
+{
+    Node* new_node = (Node*)malloc(sizeof(Node));
+    if(new_node == NULL)
+    {
+        printf("Error: Memory allocation failed for new_node\n");
+        exit(1);
+    }
+    new_node->value = value;
+    new_node->index = index;
+    new_node->next = NULL;
+
+    if(h->table[k]->first == NULL)
+    {
+        h->table[k]->first = new_node;
+    }
+    else
+    {
+        Node* temp = h->table[k]->first;
+        while(temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = new_node;
+    }
+
+    // Update final_value and count of the Sentinel node
+    if(h->table[k]->lowest_value == -1)
+    {
+        h->table[k]->lowest_value = value;
+    }
+    h->table[k]->final_value = value;
+    h->table[k]->count++;
+}
+
+//To be use only for debugging
+void print_table(HashTable* h)
+{
+    printf("Size of table: %d\n", h->size);
+    for(int i=0; i<h->size; i++)
+    {
+        printf("Sentinel node %d: final_value = %d, lowest_value = %d, count = %d\n", i, h->table[i]->final_value, h->table[i]->lowest_value, h->table[i]->count);
+        Node* temp = h->table[i]->first;
+        while(temp != NULL)
+        {
+            printf("%d ", temp->index);
+            temp = temp->next;
+        }
+        printf("\n");
+    }
+}
+
+//to print expected answer
+void print_answer(HashTable* h, int index_of_maxcount)
+{
+    Node* temp = h->table[index_of_maxcount]->first;
+    printf("%d\n", h->table[index_of_maxcount]->count);
+    while(temp != NULL)
+    {
+        printf("%d ", temp->index);
+        temp = temp->next;
+    }
+    printf("\n");
+
+}
+
+int main()
+{
+
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i =0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    HashTable* h = (HashTable*)malloc(sizeof(HashTable));
+    if(h == NULL)
+    {
+        printf("Error: Memory allocation failed for HashTable\n");
+        return 1;
+    }
+    init(h);
+    int index_of_maxcount = 0;
+    int makenew = 1;
+    int x;
+    // int used =0;    
+    for(int i=0;i<n;i++)
+    {
+        x = arr[i];
+        for(int j=0;j<(h->size);j++)
+        {
+            if(h->table[j]->lowest_value <=x && h->table[j]->final_value >= x)
+            {
+                makenew = 0;
+                continue;
+            }
+            
+            else if(h->table[j]->final_value == x-1)
+            {
+                makenew = 0;
+                insert_to_ll(x, i, j, h);
+                if(h->table[j]->count > h->table[index_of_maxcount]->count)
+                {
+                    index_of_maxcount = j;
+                }
+                break;
+            }
+        }
+        if(makenew)
+        {
+            insert_to_ll(x, i, h->size, h);
+            // if(h->table[h->size]->count == 0)
+            // {
+            //     index_of_maxcount = h->size;
+            // }
+            h->size++;
+        }
+    }
+
+    // print_table(h); 
+    print_answer(h, index_of_maxcount);
+
+    return 0;
+}
