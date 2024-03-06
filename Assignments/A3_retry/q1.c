3e053a501fedf224c06dc6b847aa7ebac8a386c8
Harshit-Lalwani <143736548+Harshit-Lalwani@users.noreply.github.com>
2024-03-06 21:32:41 +0000

    yay 15/100

--- Assignments/A3_retry/q1.c
+++ Assignments/A3_retry/q1.c
@@ -1,4 +1,4 @@
-// AIM: for a given array, find the longest subsequence of consecutive increasing numbers
+// AIM: for a given array, find the longest subsequence of consecutive increasing numbers. NOTE: A subsequence retains the order of the elements in the original array, but not necessarily the contiguity.
 
 //Constraints: 1<=n<=10^5, 1<=arr[i]<=10^9
 // LOGIC:
@@ -9,10 +9,10 @@
 // for each element in the array, check if it is the successor of the last element of a subsequence. If yes, then create a new node and add it to the linkedlist of the corresponding sentinel node. Otherwise, if element does not already exist in any of the subsequences, add it to the linkedlist of (size of table)th sentinel node and then incremkent the size of the table.
 // Finally, find the sentinel node with the maximum count and print the linkedlist of indices that sentinel node.
 
+//Note: There are multiple possible correct answers. The code must print only one of them
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 #define MAX 300000
 
@@ -96,13 +96,19 @@ void insert_to_ll(int value, int index, int k, HashTable* h)
 void print_table(HashTable* h)
 {
     printf("Size of table: %d\n", h->size);
+    if(h->size == 0)
+    {
+        printf("Table is empty\n");
+        return;
+    }
     for(int i=0; i<h->size; i++)
     {
         printf("Sentinel node %d: final_value = %d, lowest_value = %d, count = %d\n", i, h->table[i]->final_value, h->table[i]->lowest_value, h->table[i]->count);
+        
         Node* temp = h->table[i]->first;
         while(temp != NULL)
         {
-            printf("%d ", temp->index);
+            printf("%d ", temp->value);
             temp = temp->next;
         }
         printf("\n");
@@ -141,31 +147,56 @@ int main()
     }
     init(h);
     int index_of_maxcount = 0;
+
+    //one element will only be inserted to one list
+    int index_of_lowest_start = -1;
     int makenew = 1;
     int x;
     // int used =0;    
     for(int i=0;i<n;i++)
     {
+        index_of_lowest_start = -1;
+        // printf("i=%d\n--------------------------------------------------------------------------------\n", i);
+        
         x = arr[i];
         for(int j=0;j<(h->size);j++)
         {
-            if(h->table[j]->lowest_value <=x && h->table[j]->final_value >= x)
+                       
+            if(h->table[j]->final_value == x-1)
             {
+                // printf("predecessor %d found in sentinel node %d\n",x-1, j);
+                makenew = 0;          
+                
+                if(index_of_lowest_start == -1)
+                {
+                    index_of_lowest_start = j;
+                }
+                else if(h->table[j]->lowest_value < h->table[index_of_lowest_start]->lowest_value)
+                {
+                    index_of_lowest_start = j;
+                }
+
+                // printf("index_of_lowest_start = %d\n", index_of_lowest_start);
+            }
+            else if(h->table[j]->lowest_value <=x && h->table[j]->final_value >= x)
+            {
+                // printf("%d found in sentinel node %d\n",x, j);
                 makenew = 0;
                 continue;
             }
-            
-            else if(h->table[j]->final_value == x-1)
-            {
-                makenew = 0;
-                insert_to_ll(x, i, j, h);
-                if(h->table[j]->count > h->table[index_of_maxcount]->count)
-                {
-                    index_of_maxcount = j;
-                }
-                break;
-            }
         }
+
+        if(index_of_lowest_start!=-1)
+        {
+            insert_to_ll(x, i, index_of_lowest_start, h);
+            if(h->table[index_of_lowest_start]->count > h->table[index_of_maxcount]->count)
+            {
+                index_of_maxcount = index_of_lowest_start;
+            }
+
+        }       
+
+                
         if(makenew)
         {
             insert_to_ll(x, i, h->size, h);
@@ -175,6 +206,10 @@ int main()
             // }
             h->size++;
         }
+
+
+        // print_table(h);
+        // printf("make new = %d\n", makenew);
     }
 
     // print_table(h); 
